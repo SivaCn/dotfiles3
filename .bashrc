3cf1fd4e3f32edd9439b601ebe6d9dd066afaa6f
Cn Siva <cnsiva@protonmail.com>
2025-11-14 17:18:56 +0530

    Ubuntu-24.04 setup script added

--- .bashrc
+++ .bashrc
@@ -1,7 +1,4 @@
 #
-# Extend the bashrc with commonrc
-. ~/.commonrc
-
 # ~/.bashrc: executed by bash(1) for non-login shells.
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
---
+++ machine_setup_ubuntu-24.04.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+sudo apt -y update && sudo apt -y upgrade
+
+sudo apt install net-tools snap ssh terminator git wget curl
+
+sudo systemctl enable snapd ssh
+sudo systemctl start snapd ssh
+
+sudo snap install postman dbeaver-ce spotify
+sudo snap install neovim --classic
+
+cd ~
+
+```bash
+git init
+git remote add origin https://github.com/SivaCn/dotfiles3.git
+rm -rvf .bashrc .gitconfig
+
+git checkout -b tmp origin/master
+git branch -D master
+git checkout -b master origin/master
+```
+
+
+```bash
+# Clone the starter
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+# Remove the .git folder, so you can add it to your own repo later
+rm -rf ~/.config/nvim/.git
+```
+
+```bash
+sudo apt install dunst libnotify-bin
+```
+
