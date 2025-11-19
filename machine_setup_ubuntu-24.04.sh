93083fe0cac50581a37c5ee052fee1edee7d29a6
Cn Siva <cnsiva@protonmail.com>
2025-11-20 01:31:14 +0530

    changes

--- machine_setup_ubuntu-24.04.sh
+++ machine_setup_ubuntu-24.04.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo apt -y update && sudo apt -y upgrade
 
 sudo apt install net-tools snap ssh terminator git wget curl
@@ -9,11 +8,10 @@ sudo systemctl enable snapd ssh
 sudo systemctl start snapd ssh
 
 sudo snap install postman dbeaver-ce spotify
-sudo snap install neovim --classic
+sudo snap install nvim --classic
 
 cd ~
 
-```bash
 git init
 git remote add origin https://github.com/SivaCn/dotfiles3.git
 rm -rvf .bashrc .gitconfig
@@ -21,18 +19,17 @@ rm -rvf .bashrc .gitconfig
 git checkout -b tmp origin/master
 git branch -D master
 git checkout -b master origin/master
-```
 
-
-```bash
 # Clone the starter
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 # Remove the .git folder, so you can add it to your own repo later
 rm -rf ~/.config/nvim/.git
-```
 
-```bash
 sudo apt install dunst libnotify-bin
-```
 
+# Install Font Awesome (may need manual download)
+wget -O fontawesome.zip https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-desktop.zip
+unzip fontawesome.zip -d fontawesome
+sudo cp fontawesome/fontawesome-free-6.4.0-desktop/otfs/* /usr/local/share/fonts/
+sudo fc-cache -f -v
